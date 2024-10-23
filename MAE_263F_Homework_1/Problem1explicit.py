c92a7e7298831e8150e4d7c19fd571fc62603de3
Sarah Enayati <sarahenayati@Sarahs-MBP.lan>
2024-10-23 14:45:12 -0700

    Added homework1 folder

---
+++ MAE_263F_Homework_1/Problem1explicit.py
@@ -0,0 +1,520 @@
+import numpy as np
+import matplotlib.pyplot as plt
+####Code obained from course website and modified
+# Helper Function 
+def crossMat(a):
+    """
+    Returns the cross product matrix of vector 'a'.
+
+    Parameters:
+    a : np.ndarray
+        A 3-element array representing a vector.
+
+    Returns:
+    A : np.ndarray
+        The cross product matrix corresponding to vector 'a'.
+    """
+    A = np.array([[0, -a[2], a[1]],
+                  [a[2], 0, -a[0]],
+                  [-a[1], a[0], 0]])
+
+    return A
+
+# Gradient and Hessian of Elastic Energies
+def gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, l_k, EI):
+    """
+    Returns the derivative of bending energy E_k^b with respect to
+    x_{k-1}, y_{k-1}, x_k, y_k, x_{k+1}, and y_{k+1}.
+
+    Parameters:
+    xkm1, ykm1 : float
+        Coordinates of the previous node (x_{k-1}, y_{k-1}).
+    xk, yk : float
+        Coordinates of the current node (x_k, y_k).
+    xkp1, ykp1 : float
+        Coordinates of the next node (x_{k+1}, y_{k+1}).
+    curvature0 : float
+        Discrete natural curvature at node (xk, yk).
+    l_k : float
+        Voronoi length of node (xk, yk).
+    EI : float
+        Bending stiffness.
+
+    Returns:
+    dF : np.ndarray
+        Derivative of bending energy.
+    """
+
+    # Nodes in 3D
+    node0 = np.array([xkm1, ykm1, 0.0])
+    node1 = np.array([xk, yk, 0])
+    node2 = np.array([xkp1, ykp1, 0])
+
+    # Unit vectors along z-axis
+    m2e = np.array([0, 0, 1])
+    m2f = np.array([0, 0, 1])
+
+    kappaBar = curvature0
+
+    # Initialize gradient of curvature
+    gradKappa = np.zeros(6)
+
+    # Edge vectors
+    ee = node1 - node0
+    ef = node2 - node1
+
+    # Norms of edge vectors
+    norm_e = np.linalg.norm(ee)
+    norm_f = np.linalg.norm(ef)
+
+    # Unit tangents
+    te = ee / norm_e
+    tf = ef / norm_f
+
+    # Curvature binormal
+    kb = 2.0 * np.cross(te, tf) / (1.0 + np.dot(te, tf))
+
+    chi = 1.0 + np.dot(te, tf)
+    tilde_t = (te + tf) / chi
+    tilde_d2 = (m2e + m2f) / chi
+
+    # Curvature
+    kappa1 = kb[2]
+
+    # Gradient of kappa1 with respect to edge vectors
+    Dkappa1De = 1.0 / norm_e * (-kappa1 * tilde_t + np.cross(tf, tilde_d2))
+    Dkappa1Df = 1.0 / norm_f * (-kappa1 * tilde_t - np.cross(te, tilde_d2))
+
+    # Populate the gradient of kappa
+    gradKappa[0:2] = -Dkappa1De[0:2]
+    gradKappa[2:4] = Dkappa1De[0:2] - Dkappa1Df[0:2]
+    gradKappa[4:6] = Dkappa1Df[0:2]
+
+    # Gradient of bending energy
+    dkappa = kappa1 - kappaBar
+    dF = gradKappa * EI * dkappa / l_k
+
+    return dF
+
+def hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, curvature0, l_k, EI):
+    """
+    Returns the Hessian (second derivative) of bending energy E_k^b
+    with respect to x_{k-1}, y_{k-1}, x_k, y_k, x_{k+1}, and y_{k+1}.
+
+    Parameters:
+    xkm1, ykm1 : float
+        Coordinates of the previous node (x_{k-1}, y_{k-1}).
+    xk, yk : float
+        Coordinates of the current node (x_k, y_k).
+    xkp1, ykp1 : float
+        Coordinates of the next node (x_{k+1}, y_{k+1}).
+    curvature0 : float
+        Discrete natural curvature at node (xk, yk).
+    l_k : float
+        Voronoi length of node (xk, yk).
+    EI : float
+        Bending stiffness.
+
+    Returns:
+    dJ : np.ndarray
+        Hessian of bending energy.
+    """
+
+    # Nodes in 3D
+    node0 = np.array([xkm1, ykm1, 0])
+    node1 = np.array([xk, yk, 0])
+    node2 = np.array([xkp1, ykp1, 0])
+
+    # Unit vectors along z-axis
+    m2e = np.array([0, 0, 1])
+    m2f = np.array([0, 0, 1])
+
+    kappaBar = curvature0
+
+    # Initialize gradient of curvature
+    gradKappa = np.zeros(6)
+
+    # Edge vectors
+    ee = node1 - node0
+    ef = node2 - node1
+
+    # Norms of edge vectors
+    norm_e = np.linalg.norm(ee)
+    norm_f = np.linalg.norm(ef)
+
+    # Unit tangents
+    te = ee / norm_e
+    tf = ef / norm_f
+
+    # Curvature binormal
+    kb = 2.0 * np.cross(te, tf) / (1.0 + np.dot(te, tf))
+
+    chi = 1.0 + np.dot(te, tf)
+    tilde_t = (te + tf) / chi
+    tilde_d2 = (m2e + m2f) / chi
+
+    # Curvature
+    kappa1 = kb[2]
+
+    # Gradient of kappa1 with respect to edge vectors
+    Dkappa1De = 1.0 / norm_e * (-kappa1 * tilde_t + np.cross(tf, tilde_d2))
+    Dkappa1Df = 1.0 / norm_f * (-kappa1 * tilde_t - np.cross(te, tilde_d2))
+
+    # Populate the gradient of kappa
+    gradKappa[0:2] = -Dkappa1De[0:2]
+    gradKappa[2:4] = Dkappa1De[0:2] - Dkappa1Df[0:2]
+    gradKappa[4:6] = Dkappa1Df[0:2]
+
+    # Compute the Hessian (second derivative of kappa)
+    DDkappa1 = np.zeros((6, 6))
+
+    norm2_e = norm_e**2
+    norm2_f = norm_f**2
+
+    Id3 = np.eye(3)
+
+    # Helper matrices for second derivatives
+    tt_o_tt = np.outer(tilde_t, tilde_t)
+    tmp = np.cross(tf, tilde_d2)
+    tf_c_d2t_o_tt = np.outer(tmp, tilde_t)
+    kb_o_d2e = np.outer(kb, m2e)
+
+    D2kappa1De2 = (2 * kappa1 * tt_o_tt - tf_c_d2t_o_tt - tf_c_d2t_o_tt.T) / norm2_e - \
+                  kappa1 / (chi * norm2_e) * (Id3 - np.outer(te, te)) + \
+                  (kb_o_d2e + kb_o_d2e.T) / (4 * norm2_e)
+
+    tmp = np.cross(te, tilde_d2)
+    te_c_d2t_o_tt = np.outer(tmp, tilde_t)
+    tt_o_te_c_d2t = te_c_d2t_o_tt.T
+    kb_o_d2f = np.outer(kb, m2f)
+
+    D2kappa1Df2 = (2 * kappa1 * tt_o_tt + te_c_d2t_o_tt + te_c_d2t_o_tt.T) / norm2_f - \
+                  kappa1 / (chi * norm2_f) * (Id3 - np.outer(tf, tf)) + \
+                  (kb_o_d2f + kb_o_d2f.T) / (4 * norm2_f)
+    D2kappa1DeDf = -kappa1 / (chi * norm_e * norm_f) * (Id3 + np.outer(te, tf)) \
+                  + 1.0 / (norm_e * norm_f) * (2 * kappa1 * tt_o_tt - tf_c_d2t_o_tt + \
+                  tt_o_te_c_d2t - crossMat(tilde_d2))
+    D2kappa1DfDe = D2kappa1DeDf.T
+
+    # Populate the Hessian of kappa
+    DDkappa1[0:2, 0:2] = D2kappa1De2[0:2, 0:2]
+    DDkappa1[0:2, 2:4] = -D2kappa1De2[0:2, 0:2] + D2kappa1DeDf[0:2, 0:2]
+    DDkappa1[0:2, 4:6] = -D2kappa1DeDf[0:2, 0:2]
+    DDkappa1[2:4, 0:2] = -D2kappa1De2[0:2, 0:2] + D2kappa1DfDe[0:2, 0:2]
+    DDkappa1[2:4, 2:4] = D2kappa1De2[0:2, 0:2] - D2kappa1DeDf[0:2, 0:2] - \
+                         D2kappa1DfDe[0:2, 0:2] + D2kappa1Df2[0:2, 0:2]
+    DDkappa1[2:4, 4:6] = D2kappa1DeDf[0:2, 0:2] - D2kappa1Df2[0:2, 0:2]
+    DDkappa1[4:6, 0:2] = -D2kappa1DfDe[0:2, 0:2]
+    DDkappa1[4:6, 2:4] = D2kappa1DfDe[0:2, 0:2] - D2kappa1Df2[0:2, 0:2]
+    DDkappa1[4:6, 4:6] = D2kappa1Df2[0:2, 0:2]
+
+    # Hessian of bending energy
+    dkappa = kappa1 - kappaBar
+    dJ = 1.0 / l_k * EI * np.outer(gradKappa, gradKappa)
+    dJ += 1.0 / l_k * dkappa * EI * DDkappa1
+
+    return dJ
+
+def getFb(q, EI, deltaL):
+    """
+    Compute the bending force and Jacobian of the bending force.
+
+    Parameters:
+    q : np.ndarray
+        A vector of size 6 containing the coordinates [x_{k-1}, y_{k-1}, x_k, y_k, x_{k+1}, y_{k+1}].
+    EI : float
+        The bending stiffness.
+    deltaL : float
+        The Voronoi length.
+
+    Returns:
+    Fb : np.ndarray
+        Bending force (vector of size 6).
+    Jb : np.ndarray
+        Jacobian of the bending force (6x6 matrix).
+    """
+
+    # Initialize bending force as a zero vector of size 6
+    Fb = np.zeros(6)
+
+    # Extract coordinates from q
+    xkm1, ykm1, xk, yk, xkp1, ykp1 = q
+
+    # Compute the gradient of bending energy
+    gradEnergy = gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, 0, deltaL, EI)
+
+    # Update bending force
+    Fb[0:6] = Fb[0:6] - gradEnergy
+
+    # Initialize Jacobian of bending force as a 6x6 zero matrix
+    Jb = np.zeros((6, 6))
+
+    # Compute the Hessian of bending energy
+    hessEnergy = hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, 0, deltaL, EI)
+
+    # Update Jacobian matrix
+    Jb[0:6, 0:6] = Jb[0:6, 0:6] - hessEnergy
+
+    return Fb, Jb
+    # Test
+# Function to call getFb with predefined inputs
+def test_getFb():
+    # Predefined inputs
+    q = np.array([0.0062, -0.0420, 0.0500, -0.0661, 0.0938, -0.0420])
+    EI = 7.8540e-04
+    deltaL = 0.05
+
+    # Call getFb function
+    Fb, Jb = getFb(q, EI, deltaL)
+
+    # Print the outputs
+    print("Bending Force (Fb):")
+    print(Fb)
+    print("\nJacobian Matrix (Jb):")
+    print(Jb)
+
+# Call the test function
+test_getFb()
+def gradEs(xk, yk, xkp1, ykp1, l_k, EA):
+    import numpy as np
+    """
+    Calculate the gradient of the stretching energy with respect to the coordinates.
+
+    Args:
+    - xk (float): x coordinate of the current point
+    - yk (float): y coordinate of the current point
+    - xkp1 (float): x coordinate of the next point
+    - ykp1 (float): y coordinate of the next point
+    - l_k (float): reference length
+    - EA (float): elastic modulus
+
+    Returns:
+    - F (np.array): Gradient array
+    """
+    F = np.zeros(4)
+    F[0] = -(1.0 - np.sqrt((xkp1 - xk)**2.0 + (ykp1 - yk)**2.0) / l_k) * ((xkp1 - xk)**2.0 + (ykp1 - yk)**2.0)**(-0.5) / l_k * (-2.0 * xkp1 + 2.0 * xk)
+    F[1] = -(0.1e1 - np.sqrt((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k) * ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1) / l_k * (-0.2e1 * ykp1 + 0.2e1 * yk)
+    F[2] = -(0.1e1 - np.sqrt((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k) * ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1) / l_k * (0.2e1 * xkp1 - 0.2e1 * xk)
+    F[3] = -(0.1e1 - np.sqrt((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k) * ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1) / l_k * (0.2e1 * ykp1 - 0.2e1 * yk)
+
+    F = 0.5 * EA * l_k * F  # Scale by EA and l_k
+
+    return F
+
+def hessEs(xk, yk, xkp1, ykp1, l_k, EA):
+    """
+    This function returns the 4x4 Hessian of the stretching energy E_k^s with
+    respect to x_k, y_k, x_{k+1}, and y_{k+1}.
+    """
+    J = np.zeros((4, 4))  # Initialize the Hessian matrix
+    J11 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (-2 * xkp1 + 2 * xk) ** 2) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * ((-2 * xkp1 + 2 * xk) ** 2) / 0.2e1 - 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+    J12 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (-2 * ykp1 + 2 * yk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (-2 * ykp1 + 2 * yk) / 0.2e1
+    J13 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * xkp1 - 2 * xk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (2 * xkp1 - 2 * xk) / 0.2e1 + 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+    J14 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * ykp1 - 2 * yk) * (-2 * xkp1 + 2 * xk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (-2 * xkp1 + 2 * xk) * (2 * ykp1 - 2 * yk) / 0.2e1
+    J22 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (-2 * ykp1 + 2 * yk) ** 2) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * ((-2 * ykp1 + 2 * yk) ** 2) / 0.2e1 - 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+    J23 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * xkp1 - 2 * xk) * (-2 * ykp1 + 2 * yk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (-2 * ykp1 + 2 * yk) * (2 * xkp1 - 2 * xk) / 0.2e1
+    J24 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * ykp1 - 2 * yk) * (-2 * ykp1 + 2 * yk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (-2 * ykp1 + 2 * yk) * (2 * ykp1 - 2 * yk) / 0.2e1 + 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+    J33 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * xkp1 - 2 * xk) ** 2) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * ((2 * xkp1 - 2 * xk) ** 2) / 0.2e1 - 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+    J34 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * ykp1 - 2 * yk) * (2 * xkp1 - 2 * xk)) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * (2 * xkp1 - 2 * xk) * (2 * ykp1 - 2 * yk) / 0.2e1
+    J44 = (1 / ((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) / l_k ** 2 * (2 * ykp1 - 2 * yk) ** 2) / 0.2e1 + (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.3e1 / 0.2e1)) / l_k * ((2 * ykp1 - 2 * yk) ** 2) / 0.2e1 - 0.2e1 * (0.1e1 - np.sqrt(((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2)) / l_k) * (((xkp1 - xk) ** 2 + (ykp1 - yk) ** 2) ** (-0.1e1 / 0.2e1)) / l_k
+
+    J = np.array([[J11, J12, J13, J14],
+                   [J12, J22, J23, J24],
+                   [J13, J23, J33, J34],
+                   [J14, J24, J34, J44]])
+
+    J *= 0.5 * EA * l_k
+
+    return J
+def getFs(q, EA, deltaL):
+    Fs = np.zeros_like(q)  # force
+    Js = np.zeros((len(q), len(q)))  # jacobian
+
+    # force due to Es^1
+    xkm1 = q[0]
+    ykm1 = q[1]
+    xk = q[2]
+    yk = q[3]
+    gradEnergy = gradEs(xkm1, ykm1, xk, yk, deltaL, EA)  # gradEs returns the gradient of (axial stretch)^2
+    Fs[0:4] -= gradEnergy
+
+    # Jacobian due to Es^1
+    hessEnergy = hessEs(xkm1, ykm1, xk, yk, deltaL, EA)  # hessEs returns the hessian of (axial stretch)^2
+    Js[0:4, 0:4] -= hessEnergy
+
+    # force due to Es^2
+    xkm1 = q[2]
+    ykm1 = q[3]
+    xk = q[4]
+    yk = q[5]
+    gradEnergy = gradEs(xkm1, ykm1, xk, yk, deltaL, EA)  # gradEs returns the gradient of (axial stretch)^2
+    Fs[2:6] -= gradEnergy
+
+    # Jacobian due to Es^2
+    hessEnergy = hessEs(xkm1, ykm1, xk, yk, deltaL, EA)  # hessEs returns the hessian of (axial stretch)^2
+    Js[2:6, 2:6] -= hessEnergy
+
+    return Fs, Js
+    
+EA = 3.1416e+03
+deltaL = 0.05
+xk = 0.0062
+yk = -0.0420
+xkp1 = 0.0500
+ykp1 = -0.0661
+gradEnergy = gradEs(xk, yk, xkp1, ykp1, deltaL, EA)
+hessEnergy = hessEs(xk, yk, xkp1, ykp1, deltaL, EA)
+print(gradEnergy)
+print(hessEnergy)
+
+def explicit_update(q_ne, q, u, m, EI, EA, deltaL, dt, W, C):
+
+    Fb, _ = getFb(q, EI, deltaL) #bending force
+    Fs, _ = getFs(q, EA, deltaL) #elastic force
+    Fv = -C @ u #viscous force
+    print('u:', u )
+    print(f"Fb: {Fb[3]:.6f} Fs: {Fs[3]:.6f} Fv: {Fv[3]:.6f} W: {W[3]:.6f}")
+
+    F_net = Fb + Fs + Fv + W  #sum of forces
+    a = F_net / m #acceleration 
+    #q_new = q + dt * u  #+ (dt**2) * a #update position ???? IS IT .5???
+    #u_new = u + dt * a #update velocity 
+    u_new = u + dt * a
+    q_new = q + dt * u_new
+
+    return q_new, u_new
+
+# Inputs (SI units)
+# number of vertices
+nv = 3
+
+# Time step
+dt = 1e-2
+
+# Rod Length
+RodLength = 0.10
+
+# Discrete length
+deltaL = RodLength / (nv - 1)
+
+# Radius of spheres
+R1 = 0.005
+R2 = 0.025
+R3 = 0.005
+
+# Densities
+rho_metal = 7000
+rho_gl = 1000
+rho = rho_metal - rho_gl
+
+# Cross-sectional radius of rod
+r0 = 1e-3
+
+# Young's modulus
+Y = 1e9
+
+# Viscosity
+visc = 1000.0
+
+# Total simulation time (it exits after t=totalTime)
+totalTime = 10
+
+# Indicate whether images should be saved
+saveImage = 0
+
+# How often the plot should be saved?
+plotStep = 50
+
+# Utility quantities
+ne = nv - 1
+EI = Y * np.pi * r0**4 / 4
+EA = Y * np.pi * r0**2
+
+# Tolerance on force function
+tol = EI / RodLength**2 * 1e-3  # small enough force that can be neglected
+
+# Geometry of the rod
+nodes = np.zeros((nv, 2))
+for c in range(nv):
+    nodes[c, 0] = c * RodLength / ne
+
+# Compute Mass
+m = np.zeros(2 * nv)
+m[0:2] = 4 / 3 * np.pi * R1**3 * rho_metal
+m[2:4] = 4 / 3 * np.pi * R2**3 * rho_metal
+m[4:6] = 4 / 3 * np.pi * R3**3 * rho_metal
+
+mMat = np.diag(m)  # Convert into a diagonal matrix
+
+# Gravity
+W = np.zeros(2 * nv)
+g = np.array([0, -9.8])  # m/s^2 - gravity
+W[0:2] = 4 / 3 * np.pi * R1**3 * rho * g
+W[2:4] = 4 / 3 * np.pi * R2**3 * rho * g
+W[4:6] = 4 / 3 * np.pi * R3**3 * rho * g
+
+# Viscous damping matrix, C
+C = np.zeros((2 * nv, 2 * nv))
+C1 = 6 * np.pi * visc * R1
+C2 = 6 * np.pi * visc * R2
+C3 = 6 * np.pi * visc * R3
+C[0, 0] = C1
+C[1, 1] = C1
+C[2, 2] = C2
+C[3, 3] = C2
+C[4, 4] = C3
+C[5, 5] = C3
+
+# Initial conditions
+q0 = np.zeros(2 * nv)
+for c in range(nv):
+    q0[2 * c] = nodes[c, 0]
+    q0[2 * c + 1] = nodes[c, 1]
+
+q = q0.copy()
+u = (q - q0) / dt
+
+# Number of time steps
+Nsteps = round(totalTime / dt)
+print('Nsteps:', Nsteps)
+ctime = 0
+
+all_pos = np.zeros(Nsteps)
+all_v = np.zeros(Nsteps)
+
+
+#all_pos = np.zeros((Nsteps, len(q))) # Store positions 
+#all_v = np.zeros((Nsteps, len(q))) # Store velocities 
+
+
+# Run the simulation
+q_new = q
+u_new = u
+for i in range(Nsteps):
+    print(f"q: {q[3]:.6f} u: {u[3]:.6f}")
+    q_new, u_new = explicit_update(q_new, q, u, m, EI, EA, deltaL, dt, W, C)
+    all_pos[i] = q_new[3]  
+    all_v[i] = u_new[3]    
+    q, u = q_new, u_new 
+    ctime += dt  # Increment time  
+    
+t = np.linspace(0, totalTime, Nsteps)
+
+# Plotting displacement and velocity
+plt.figure(figsize=(12, 5))
+# Displacement plots
+plt.subplot(1, 2, 1)
+plt.plot(t, all_pos, color='k')  
+plt.title('Displacement for the Explicit Method')
+plt.xlabel('Time (s)')
+plt.ylabel('Displacement (m)')
+
+# Velocity plot
+plt.subplot(1, 2, 2)
+plt.plot(t, all_v, color = 'k')  # Plot the first three elements as an example
+plt.title('Velocity for Explicit Method')
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m/s)')
+
+plt.tight_layout()
+plt.show()
